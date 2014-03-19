b9281fb023fa91e40de5ea7331befaabe46d9789
Cameron Martin <cameronmartin123@gmail.com>
2014-03-19 18:25:32 +0000

    More files

---
+++ lib/lol_static/item.rb
@@ -0,0 +1,21 @@
+module LolStatic
+  class Item
+
+    def self.api_version
+      @api_version ||= LolStatic.api_version('item')
+    end
+
+    def initialize(item_id)
+      @item_id = item_id
+    end
+
+    def api_version
+      self.class.api_version
+    end
+
+    def image_url
+      "#{LolStatic.base_uri}/#{api_version}/img/item/#{@item_id}.png"
+    end
+
+  end
+end
